9df13c50a44f26af787afd029b5b934ee2c0a1c8
ahmetfbas <48409896+ahmetfbas@users.noreply.github.com>
2026-01-19 22:58:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import os
 import requests
 from datetime import datetime
 
 def get_todays_games():
     today = datetime.utcnow().strftime("%Y-%m-%d")
 
+    api_key = os.getenv("BALLDONTLIE_API_KEY")
+
+    if not api_key:
+        print("Missing API key.")
+        return []
+
     url = "https://api.balldontlie.io/v1/games"
+    headers = {
+        "Authorization": api_key
+    }
     params = {
         "dates[]": today,
         "per_page": 100
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code != 200:
         print("API error:", response.status_code)
